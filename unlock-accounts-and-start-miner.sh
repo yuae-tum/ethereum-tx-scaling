a20b28c6d2cf7d1243c2e0964e15e97d40f689e7
yuae <yuae710@gmail.com>
2023-01-18 11:16:49 +0100

    change shell script to allow soution run coorectly

--- unlock-accounts-and-start-miner.sh
+++ unlock-accounts-and-start-miner.sh
@@ -5,5 +5,6 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-docker exec $1 geth attach /geth/geth.ipc --preload "/unlock-accounts.js,/start-mining.js"
+docker exec $1 geth --exec 'loadScript("/unlock-accounts.js")' attach ipc:/geth/geth.ipc
+docker exec $1 geth --exec 'loadScript("/start-mining.js")' attach ipc:/geth/geth.ipc
 docker exec $1 sh -c "cd /truffle && truffle deploy"
